7ef494fe60284048d7219d65d8ee1a9ca9b2a56c
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-24 13:07:34 +0300

    Routes fix

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
+require("dotenv").config();
+
 const express = require("express");
+const userRoutes = require("./routes/user");
 
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
+app.use(express.json());
 
-app.get("/login", (req, res) => {
-  res.send("Welcome to my login!");
-});
+app.use("/api/user", userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.use("/api/workouts", workoutRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen on port
+    app.listen(process.env.PORT, () => {
+      console.log("Listening for requests on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my server!");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.send("Welcome to my login!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// const express = require("express");
+
+// //express app
+// const app = express();
+// const mongoose = require("mongoose");
+
+// app.use(express.json());
+
+// app.use("/api/workouts", workoutRoutes);
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     //listen on port
+//     app.listen(process.env.PORT, () => {
+//       console.log("Listening for requests on port", process.env.PORT);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
