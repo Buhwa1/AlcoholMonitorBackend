49d23832b802a79564892c8399c70c3106b9a8fd
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-24 12:36:46 +0300

    Login

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to my server!");
 });
 
+app.get("/login", (req, res) => {
+  res.send("Welcome to my login!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
