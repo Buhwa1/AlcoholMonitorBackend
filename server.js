a3ff0f887cc078e35b863e13b6bf8a161e638e83
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-24 22:38:03 +0300

    Fix

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const userRoutes = require("./src/routes/user");
-
+const jwt = require("jsonwebtoken");
+const authenticateToken = require("./src/Middleware/authenticationToken");
 const app = express();
 const mongoose = require("mongoose");
 
