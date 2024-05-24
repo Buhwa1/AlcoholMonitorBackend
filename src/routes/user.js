f572f90c493ca9cf5459fccbd26159203e51e21b
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-24 21:57:55 +0300

    Login endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,8 @@ const {
   createUser,
   deleteUser,
   updateUser,
+  loginUser,
+  getUserFromToken,
 } = require("../Controllers/UserController");
 const router = express.Router();
 
@@ -17,5 +19,9 @@ router.post("/create", createUser);
 router.delete("/delete/:id", deleteUser);
 
 router.patch("/update/:id", updateUser);
+
+router.post("/login", loginUser);
+
+router.get("/single", getUserFromToken);
 
 module.exports = router;
