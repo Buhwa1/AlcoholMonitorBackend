39ebe49c3610a1f9a34a742b8ab00fb27e73e880
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-24 22:24:01 +0300

    Secure auth

---
+++ src/Token/authenticationToken.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["Authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      message: "Token not provided",
+      status: "FAIL",
+      details: "No authorization token was found",
+    });
+  }
+
+  jwt.verify(token, "monitor@userapp", (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        message: "Token is invalid",
+        status: "FAIL",
+        details: "The provided token is not valid",
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
