368142c6f0d882bd43eb599cb00454b4d46c417c
EzraBuhwa <ezybuhwa@gmail.com>
2024-05-25 11:09:51 +0300

    Fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,17 +14,13 @@ const authenticateToken = require("../Middleware/authenticationToken");
 const router = express.Router();
 
 router.post("/login", loginUser);
-
-router.get("/single", fetchSingleUserFromToken);
-
+router.get("/single", authenticateToken, fetchSingleUserFromToken);
 router.post("/create", createUser);
-
 router.get("/list", authenticateToken, getUsers);
 
+// General routes last
 router.get("/:id", authenticateToken, getUser);
-
 router.delete("/delete/:id", authenticateToken, deleteUser);
-
 router.patch("/update/:id", authenticateToken, updateUser);
 
 module.exports = router;
