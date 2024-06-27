87543faa2f86d02f8dd84d9189e62d13153a507f
EzraBuhwa <ezybuhwa@gmail.com>
2024-06-27 12:17:16 +0300

    Alcohol readings

--- src/routes/alcohol.js
+++ src/routes/alcohol.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   createAlcoholReading,
   fetchAlcoholReadingFromToken,
+  getReadings,
 } = require("../Controllers/AlcoholController");
 
 const authenticateToken = require("../Middleware/authenticationToken");
