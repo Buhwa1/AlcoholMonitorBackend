b53e8be27b494b5d31ceb5488be6a44f1ed2a911
EzraBuhwa <ezybuhwa@gmail.com>
2024-06-27 12:12:51 +0300

    Alcohol readings

--- src/routes/alcohol.js
+++ src/routes/alcohol.js
@@ -10,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", createAlcoholReading);
 router.get("/get", authenticateToken, fetchAlcoholReadingFromToken);
+router.get("/list", authenticateToken, getReadings);
 
 module.exports = router;
